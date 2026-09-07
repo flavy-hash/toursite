46ad46c2a02de476282decb5ff4457ae70a78591
flavy-hash <flavianmichael663@gmail.com>
2026-09-07 16:44:07 +0300

    updasting the route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,31 @@
 <?php
 
+use App\Http\Controllers\InquiryController;
+use App\Http\Controllers\ReviewController;
+use App\Http\Controllers\SitemapController;
+use App\Http\Controllers\SubscriberController;
+use App\Http\Controllers\UnsubscribeController;
+use App\Http\Controllers\TourController;
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'home')->name('home');
+
+Route::get('/tours', [TourController::class, 'index'])->name('tours.index');
+
+Route::get('/tours/{slug}', [TourController::class, 'show'])
+    ->where('slug', '[a-z0-9-]+')
+    ->name('tours.show');
+
+Route::get('/inquiry', [InquiryController::class, 'create'])->name('inquiry.create');
+Route::post('/inquiry', [InquiryController::class, 'store'])->name('inquiry.store');
+
+Route::get('/reviews', [ReviewController::class, 'index'])->name('reviews.index');
+Route::post('/reviews', [ReviewController::class, 'store'])->name('reviews.store');
+
+Route::post('/subscribe', [SubscriberController::class, 'store'])->name('subscribe');
+
+Route::get('/unsubscribe/{subscriber}', UnsubscribeController::class)
+    ->name('unsubscribe')
+    ->middleware('signed');
+
+Route::get('/sitemap.xml', SitemapController::class)->name('sitemap');
